b00a85d2ed6df3fe9f06531c7e74c9a883987b48
cristianvia <cristianviar@gmail.com>
2020-12-28 18:07:33 +0100

    change icons colors with reusable sass variables

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -70,7 +70,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
                with font-awesome or any other icon font library -->
                         <li class="nav-item">
                             <router-link to="/dashboard" class="nav-link">
-                                <i class="nav-icon fas fa-tachometer-alt"></i>
+                                <i class="nav-icon fas fa-tachometer-alt blue"></i>
                                 <p>
                                     Dashboard
                                 </p>
@@ -79,7 +79,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
 
                         <li class="nav-item has-treeview">
                             <a href="#" class="nav-link">
-                                <i class="nav-icon fas fa-cog"></i>
+                                <i class="nav-icon fas fa-cog green"></i>
                                 <p>
                                     Management
                                     <i class="right fas fa-angle-left"></i>
@@ -102,7 +102,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
 
                         <li class="nav-item">
                             <router-link to="/profile" class="nav-link">
-                                <i class="nav-icon fas fa-user"></i>
+                                <i class="nav-icon fas fa-user orange"></i>
                                 <p>
                                     Profile
                                 </p>
@@ -110,12 +110,15 @@ scratch. This page gets rid of all links and provides the needed markup only.
                         </li>
 
                         <li class="nav-item">
-                            <a href="#" class="nav-link">
-                                <i class="nav-icon fas fa-power-off"></i>
-                                <p>
-                                    Logout
-                                </p>
+                            <a class="nav-link" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                <i class="nav-icon fas fa-power-off red"></i>
+                                <p>{{ __('Logout') }}</p>
                             </a>
+
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                @csrf
+                            </form>
                         </li>
 
                         </li>
