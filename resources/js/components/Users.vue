85026f3caba1dc17b10d76d32565c2483376d589
cristianvia <cristianviar@gmail.com>
2020-12-29 10:57:58 +0100

    Change submit button type

--- resources/js/components/Users.vue
+++ resources/js/components/Users.vue
@@ -134,7 +134,7 @@
             <button type="button" class="btn btn-danger" data-dismiss="modal">
               Close
             </button>
-            <button type="button" class="btn btn-primary">Create</button>
+            <button type="submit" class="btn btn-primary">Create</button>
           </div>
           </form>
         </div>
