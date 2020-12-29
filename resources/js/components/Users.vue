69378d335039d7e85866005ee37227bc2f916a91
cristianvia <cristianviar@gmail.com>
2020-12-29 13:01:42 +0100

    add filters in v-for table

--- resources/js/components/Users.vue
+++ resources/js/components/Users.vue
@@ -32,10 +32,10 @@
               <tbody>
                 <tr v-for="user in users" :key="user.id">
                   <td>{{ user.id }}</td>
-                  <td>{{ user.name }}</td>
+                  <td>{{ user.name | upText }}</td>
                   <td>{{ user.email }}</td>           
                   <td>{{ user.type }}</td>
-                  <td>{{ user.created_at }}</td>
+                  <td>{{ user.created_at | myDate}}</td>
                   <td>
                     <a href="">
                       <i class="fa fa-edit blue"></i>
